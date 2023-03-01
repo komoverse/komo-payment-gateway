a215788748ef28cede290d96e8c605f2c19577bb
Nikko Agustino <nikkoagustino@gmail.com>
2023-03-01 10:30:40 +0700

    initial commit

---
+++ config/komo.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'KOMO_WALLET' => env('KOMO_WALLET', false),
+    'KOMO_PRIVATE_KEY' => env('KOMO_PRIVATE_KEY', false),
+    'KOMO_TOKEN_ADDRESS' => env('KOMO_TOKEN_ADDRESS', false),
+];
---
+++ config/pazemo.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'API_KEY' => env('PAZEMO_API_KEY', false),
+    'ACCOUNT_ID' => env('PAZEMO_ACCOUNT_ID', false),
+    'ENDPOINT' => env('PAZEMO_ENDPOINT', false),
+    'VA_BANK_ID' => env('PAZEMO_VA_BANK_ID', false),
+];
